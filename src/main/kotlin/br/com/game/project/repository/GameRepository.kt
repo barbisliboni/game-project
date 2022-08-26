5bef503f4e4e29ac608742cc53dec90323db7826
barbara liboni <niinaliboni@gmail.com>
2022-08-26 00:15:33 -0300

    deleting files from main branch

--- src/main/kotlin/br/com/game/project/repository/GameRepository.kt
+++
@@ -1,9 +0,0 @@
-package br.com.game.project.repository
-
-import br.com.game.project.model.Game
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface GameRepository : JpaRepository<Game, Long> {
-}
