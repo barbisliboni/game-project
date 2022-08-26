97a4c6edd14249a86e5e2c4d7baba9efe9a508d7
barbara liboni <niinaliboni@gmail.com>
2022-08-25 23:58:39 -0300

    changing class name

--- src/test/kotlin/br/com/project/game/GameApplicationTests.kt
+++ src/test/kotlin/br/com/project/game/GameApplicationTests.kt
@@ -1,10 +1,10 @@
-package br.com.project.store
+package br.com.project.game
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class GameApplicationTests {
 
 	@Test
 	fun contextLoads() {
